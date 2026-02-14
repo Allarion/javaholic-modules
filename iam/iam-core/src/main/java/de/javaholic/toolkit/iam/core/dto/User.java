e0f1c2aa566c378d564e511bf59112ddf5282f08
Allarion <Allarion@allevil.de>
2026-02-14 21:29:29 +0100

    todo (add iam.dto layer, cleanest DDD)

---
+++ iam/iam-core/src/main/java/de/javaholic/toolkit/iam/core/dto/User.java
@@ -0,0 +1,4 @@
+package de.javaholic.toolkit.iam.core.dto;
+// TODO: impl dto proxy class (DDD, hex). put the @UiAnnotations here. ; Also need a Testclass to do this all most barebone way JPAEntity uses @UiAnnotation. Also vgl. CrudMode
+public class User {
+}
